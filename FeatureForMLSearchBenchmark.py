5db63cf59f9932e7eb335ec8b92104fac793380f
Mahdi Esmailoghli <masmaillogli@gmail.com>
2023-10-02 12:06:12 +0200

    test code added

--- FeatureForMLSearchBenchmark.py
+++ FeatureForMLSearchBenchmark.py
@@ -11,11 +11,11 @@ query[2] = pd.to_numeric(query[2], errors='coerce')
 
 task = FeatureForMLSearch(query, 0, 1, 2, 10)
 
-result_ids = task.run()
-# result_ids = [73618111] # , 140502337, 10977367, 10977367, 10977367, 15400287, 140502336, 10977367, 73618056, 59921649]
+# result_ids = task.run()
+result_ids = [73618139] # , 73618111, 140502337, 10977367, 10977367, 10977367, 15400287, 140502336, 10977367, 73618056, 59921649]
 results = []
-print(result_ids)
-exit(0)
+# print(result_ids)
+# exit(0)
 for result_id in result_ids:
     results.append(task.DB.get_table_from_index(result_id))
 
