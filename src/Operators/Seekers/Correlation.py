93560cde9d3be5156089e83addf3d13697862116
Mahdi Esmailoghli <masmaillogli@gmail.com>
2023-10-08 23:55:57 +0200

    featurediscovery4ml updated

--- src/Operators/Seekers/Correlation.py
+++ src/Operators/Seekers/Correlation.py
@@ -16,7 +16,7 @@ class Correlation(Seeker):
         grouped = pd.DataFrame({'source': source_values, 'target': target_values}).dropna().groupby('source').mean()
         self.input_source = grouped.index.values
         self.input_target = grouped['target'].values
-        self.hash_size = 10
+        self.hash_size = 5
 
         self.base_sql = f"""
         SELECT TableId
